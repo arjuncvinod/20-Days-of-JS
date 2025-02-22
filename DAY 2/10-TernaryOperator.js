194be306101d9d395723cf917eb119dccdf46598
arjuncvinod <arjuncvinod007@gmail.com>
2025-02-23 01:35:56 +0530

    Add Day 2 JavaScript exercises for ternary operators, switch statements, and template literals

---
+++ DAY 2/10-TernaryOperator.js
@@ -0,0 +1,5 @@
+let num1 = 11
+let result
+
+result =num1%2===0 ?"even":"odd"
+console.log(result)
---
+++ DAY 2/12-TemplateLiteral.js
@@ -0,0 +1,4 @@
+let num1 = 10
+let num2 = 5
+let result = num1 + num2
+console.log(`The additon of ${num1} and ${num2} is ${result}`) 
