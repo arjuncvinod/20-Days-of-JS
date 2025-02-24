a0114137b41b5d96d2030b2b11c4b128a86395a8
arjuncvinod <arjuncvinod007@gmail.com>
2025-02-25 01:24:01 +0530

    Add Day 3 JavaScript exercises for functions and object methods

---
+++ DAY 3/18-LocalGlobal.js
@@ -0,0 +1,18 @@
+// let user = 'arjun';
+// function greet(u) {
+//     let num = 5;
+//     console.log(num);
+//     return `Hello ${u}!!`
+// }
+// console.log("num value is " + num);
+// let str = greet(user);
+// console.log(str);
+
+
+function add (num1,num2, num3=1)
+{
+console.log(num1, num2, num3);
+return num1 + num2 + num3;
+}
+let result = add(5,6);
+console.log(result);
---
+++ DAY 3/19-Methods.js
@@ -0,0 +1,9 @@
+let laptop = {
+    cpu: 'i7',
+    ram: 4,
+    brand: 'Asus',
+    greet: function () {
+        console.log('Hello World');
+    }
+}
+laptop.greet();
